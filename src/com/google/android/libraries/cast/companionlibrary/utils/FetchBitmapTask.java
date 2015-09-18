81f7db2e9e444729692ba01002d4eef814693f10
Oliver Budiardjo <oliver@budiardjo.com>
2015-09-18 14:43:25 -0400

    Ssl certificate bypass for chromecast image downloading

--- src/com/google/android/libraries/cast/companionlibrary/utils/FetchBitmapTask.java
+++ src/com/google/android/libraries/cast/companionlibrary/utils/FetchBitmapTask.java
@@ -30,6 +30,15 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.security.cert.CertificateException;
+import javax.security.cert.X509Certificate;
+
 /**
  * An AsyncTask to fetch an image over HTTP and scale it to the desired size. Clients need to extend
  * this and implement their own {@code onPostExecute(Bitmap bitmap)} method. It provides a uniform
@@ -72,7 +81,14 @@ public abstract class FetchBitmapTask extends AsyncTask<Uri, Void, Bitmap> {
         }
         HttpURLConnection urlConnection = null;
         try {
-            urlConnection = (HttpURLConnection) url.openConnection();
+            if (url.getProtocol().toLowerCase().equals("https")) {
+                trustAllHosts();
+                HttpsURLConnection https = (HttpsURLConnection) url.openConnection();
+                https.setHostnameVerifier(DO_NOT_VERIFY);
+                urlConnection = https;
+            } else {
+                urlConnection = (HttpURLConnection) url.openConnection();
+            }
             urlConnection.setDoInput(true);
 
             if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
@@ -90,6 +106,45 @@ public abstract class FetchBitmapTask extends AsyncTask<Uri, Void, Bitmap> {
         }
 
         return bitmap;
+    }
+
+    // always verify the host - dont check for certificate
+    final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    };
+
+    /**
+     * Trust every server - dont check for any certificate
+     */
+    private static void trustAllHosts() {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[] {};
+            }
+
+            @Override
+            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws java.security.cert.CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws java.security.cert.CertificateException {
+
+            }
+        } };
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection
+                    .setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
